25034040d51d7f349263edd788137f18da832a02
Kenneth Chang <kenifred@kenneths-pro.lan>
2024-10-18 04:54:58 -0700

    Default dummy images added to use before database is set up

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,13 +1,22 @@
 import { Tabs } from 'expo-router';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import {FontAwesome} from '@expo/vector-icons'
+
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>['name'];
+  color: string;
+}) {
+  return <FontAwesome size={24} {...props} style={{ color: '#D9D5EC' }} />;
+}
 
 const TabsLayout = () => {
   return (
     <SafeAreaView edges={['top']} style={styles.safeArea} >
       <Tabs 
         screenOptions={{
-          tabBarActiveTintColor: '#BC4997',
+          tabBarActiveTintColor: '#D9D5EC',
+          // Change out the gray color scheme later
           tabBarInactiveTintColor: 'gray',
           tabBarLabelStyle: { fontSize: 16 },
           tabBarStyle: {
@@ -21,12 +30,22 @@ const TabsLayout = () => {
         <Tabs.Screen 
           name="index" 
           options={{
-            headerShown: false,
+            // headerShown: false,
+            title: 'shop',
+            tabBarIcon(props) {
+              return <TabBarIcon {...props} name='shopping-cart' />
+            }
           }} 
         />
         <Tabs.Screen 
           name="orders" 
-          options={{}} 
+          options={{
+            // headerShown: false,
+            title: 'orders',
+            tabBarIcon(props) {
+              return <TabBarIcon {...props} name='book' />
+            }
+          }} 
         />
       </Tabs>
     </SafeAreaView>
