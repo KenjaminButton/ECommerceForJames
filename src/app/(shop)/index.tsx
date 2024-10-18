483bda71d96222ff63a6c54148af5ba3a8f2a26a
Kenneth Chang <kenifred@kenneths-pro.lan>
2024-10-18 06:50:24 -0700

    Hero Image updated to fit homepage vibe

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -2,6 +2,7 @@ import { FlatList, StyleSheet, Text, View } from 'react-native'
 import { Tabs } from "expo-router"
 import { PRODUCTS } from '../../../assets/products'
 import {ProductListItem} from '../../components/product-list-item'
+import { ListHeader } from '../../components/list-header'
 
 const Home = () => {
   return (
@@ -11,7 +12,7 @@ const Home = () => {
         renderItem={({ item }) => <ProductListItem product={item} />}
         keyExtractor={item => item.id.toString()}
         numColumns={2}
-        ListHeaderComponent={<Text>Products</Text>}
+        ListHeaderComponent={ListHeader}
         contentContainerStyle={styles.flatListContent}
         columnWrapperStyle={styles.flatListColumn}
         style={{paddingHorizontal: 10, paddingVertical: 5}}
