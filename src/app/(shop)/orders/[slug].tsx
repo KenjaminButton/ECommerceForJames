8d96e9062f7b4514b25815e81d0e67d62d4ed937
Kenneth Chang <kenifred@kenneths-pro.lan>
2024-10-24 13:48:48 -0700

    ORder details product images loaded and item price

--- src/app/(shop)/orders/[slug].tsx
+++ src/app/(shop)/orders/[slug].tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Redirect, Stack, useLocalSearchParams } from 'expo-router'
 import { ORDERS } from '../../../../assets/orders'
@@ -17,6 +17,27 @@ const OrderDetails = () => {
     <View>
       <Stack.Screen options={{title: `${order.item}`}}/>
       <Text style={styles.item}>{order.item}</Text>
+      <Text style={styles.details}>{order.details}</Text>
+      <View style={[styles.statusBadge, styles[`statusBadge_${order.status}`]]}>
+        <Text style={styles.statusText}>{order.status}</Text>
+      </View>
+      <Text style={styles.date}>{order.date}</Text>
+      <Text style={styles.itemsTitle}>Items Ordered: </Text>
+      <FlatList
+        data={order.items}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item }) => (
+          <View style={styles.orderItem}>
+            <Image source={item.heroImage} style={styles.heroImage} />
+            <View style={styles.itemInfo}>
+              <Text style={styles.itemName}>{item.title}</Text>
+              <Text style={styles.itemPrice}>Price: ${item.price}</Text>
+            </View>
+          </View>
+        )}
+      >
+
+      </FlatList>
     </View>
   )
 }
