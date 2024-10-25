d62706bc0a8c2f62edcc75eb3b49edf75e88dca7
KenjaminButton <kennethpchang@gmail.com>
2024-10-25 00:22:47 -0700

    Hero Image updated to fit homepage vibe

--- src/components/product-list-item.tsx
+++ src/components/product-list-item.tsx
@@ -1,19 +1,21 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Product } from '../../assets/types/product'
+import { Link } from 'expo-router'
 
 export const ProductListItem = ({ product }: {product: Product}) => {
   return (
-    <View>
-      {/* <Text>ProductListItem</Text> */}
-      <View style={styles.itemImageContainer}>
-        <Image source={product.heroImage} style={styles.itemImage} />
-      </View>
-      <View style={styles.itemTextContainer}>
-        <Text style={styles.itemTitle}>{product.title}</Text>
-        <Text style={styles.itemPrice}>${product.price.toFixed(2)}</Text>
-      </View>
-    </View>
+    <Link asChild href={`/product/${product.slug}`}>
+      <Pressable style={styles.item}>
+        <View style={styles.itemImageContainer}>
+          <Image source={product.heroImage} style={styles.itemImage} />
+        </View>
+        <View style={styles.itemTextContainer}>
+          <Text style={styles.itemTitle}>{product.title}</Text>
+          <Text style={styles.itemPrice}>${product.price.toFixed(2)}</Text>
+        </View>
+      </Pressable>
+    </Link>
   )
 }
 
